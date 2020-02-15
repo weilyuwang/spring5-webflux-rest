af08e4e38351638846f578f5e2f1c9b496072cd8
weilyuwang <weilyuwang@gmail.com>
2020-02-15 16:18:23 -0500

    create category controller

---
+++ src/main/java/weilyu/springframework/springwebfluxrest/controllers/CategoryController.java
@@ -0,0 +1,31 @@
+package weilyu.springframework.springwebfluxrest.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import weilyu.springframework.springwebfluxrest.domain.Category;
+import weilyu.springframework.springwebfluxrest.repository.CategoryRepository;
+
+@RestController
+public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("/api/v1/categories")
+    Flux<Category> list() {
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/api/v1/categories/{id}")
+    Mono<Category> getById(@PathVariable String id) {
+        return categoryRepository.findById(id);
+    }
+
+}
