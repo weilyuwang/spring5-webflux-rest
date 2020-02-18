e5f6946d99a3620b9010c5db24d7dc676ba583b9
weilyuwang <weilyuwang@gmail.com>
2020-02-17 19:14:10 -0500

    create the PATCH controller method and unit tests for Category

--- src/main/java/weilyu/springframework/springwebfluxrest/controllers/CategoryController.java
+++ src/main/java/weilyu/springframework/springwebfluxrest/controllers/CategoryController.java
@@ -34,10 +34,26 @@ public class CategoryController {
         return categoryRepository.saveAll(categoryStream).then();
     }
 
+    // When using PUT request we have to send all parameter
     @PutMapping("/api/v1/categories/{id}")
     Mono<Category> update(@PathVariable String id, @RequestBody Category category) {
         category.setId(id);
         return categoryRepository.save(category);
+    }
+
+    // Where as In patch request only send the parameters which one we want to update and it won't effecting or changing other data
+    @PatchMapping("/api/v1/categories/{id}")
+    Mono<Category> patch(@PathVariable String id, @RequestBody Category category) {
+
+        Category foundCategory = categoryRepository.findById(id).block();
+
+        assert foundCategory != null;
+        if(!foundCategory.getDescription().equals(category.getDescription())){  // if description field is changed, update it
+            foundCategory.setDescription(category.getDescription());
+            return categoryRepository.save(foundCategory);
+        }
+
+        return Mono.just(foundCategory);   // if no changes made, just return the existing one.
     }
 
 }
--- src/test/java/weilyu/springframework/springwebfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/weilyu/springframework/springwebfluxrest/controllers/CategoryControllerTest.java
@@ -2,8 +2,6 @@ package weilyu.springframework.springwebfluxrest.controllers;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.BDDMockito;
-import org.mockito.Mockito;
 import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
@@ -11,8 +9,10 @@ import reactor.core.publisher.Mono;
 import weilyu.springframework.springwebfluxrest.domain.Category;
 import weilyu.springframework.springwebfluxrest.repository.CategoryRepository;
 
-import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 public class CategoryControllerTest {
 
@@ -22,14 +22,14 @@ public class CategoryControllerTest {
 
     @Before
     public void setUp() {
-        categoryRepository = Mockito.mock(CategoryRepository.class);
+        categoryRepository = mock(CategoryRepository.class);
         categoryController = new CategoryController(categoryRepository);
         webTestClient = WebTestClient.bindToController(categoryController).build();
     }
 
     @Test
     public void list() {
-        BDDMockito.given(categoryRepository.findAll())
+        given(categoryRepository.findAll())
                 .willReturn(Flux.just(
                         Category.builder().description("Cat1").build(),
                         Category.builder().description("Cat2").build()));
@@ -41,7 +41,7 @@ public class CategoryControllerTest {
 
     @Test
     public void getById() {
-        BDDMockito.given(categoryRepository.findById("someid"))
+        given(categoryRepository.findById("someid"))
                 .willReturn(Mono.just(Category.builder().description("Cat").build()));
 
         webTestClient.get().uri("/api/v1/categories/someid")
@@ -51,7 +51,7 @@ public class CategoryControllerTest {
 
     @Test
     public void testCreateCategory() {
-        BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
+        given(categoryRepository.saveAll(any(Publisher.class)))
                 .willReturn(Flux.just(Category.builder().description("descrp").build()));
 
         Mono<Category> catToSaveMono = Mono.just(Category.builder().description("Cat").build());
@@ -66,7 +66,7 @@ public class CategoryControllerTest {
 
     @Test
     public void testUpdate() {
-        BDDMockito.given(categoryRepository.save(any(Category.class)))
+        given(categoryRepository.save(any(Category.class)))
                 .willReturn(Mono.just(Category.builder().build()));
 
         Mono<Category> catToUpdateMono = Mono.just(Category.builder().description("Cat").build());
@@ -77,5 +77,45 @@ public class CategoryControllerTest {
                 .exchange()
                 .expectStatus()
                 .isOk();
+    }
+
+    @Test
+    public void testPatchWithChanges() {
+        given(categoryRepository.findById(anyString()))
+                .willReturn(Mono.just(Category.builder().description("description").build()));
+
+        given(categoryRepository.save(any(Category.class)))
+                .willReturn(Mono.just(Category.builder().description("description").build()));
+
+        Mono<Category> catToUpdateMono = Mono.just(Category.builder().description("New Description").build());
+
+        webTestClient.patch()
+                .uri("/api/v1/categories/someId")
+                .body(catToUpdateMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        verify(categoryRepository).save(any());
+    }
+
+    @Test
+    public void testPatchNoChanges() {
+        given(categoryRepository.findById(anyString()))
+                .willReturn(Mono.just(Category.builder().description("description").build()));
+
+        given(categoryRepository.save(any(Category.class)))
+                .willReturn(Mono.just(Category.builder().description("description").build()));
+
+        Mono<Category> catToUpdateMono = Mono.just(Category.builder().description("description").build());
+
+        webTestClient.patch()
+                .uri("/api/v1/categories/someId")
+                .body(catToUpdateMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
+        verify(categoryRepository, never()).save(any());
     }
 }
